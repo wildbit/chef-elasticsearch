5ee074039815a9436606092b6b72c9f0c20aa7f7
Michael Carruthers <michael.carruthers@wildbit.com>
2015-01-13 11:43:05 -0800

    delete extraneous cookbook directory

--- cookbooks/wb-java/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-set[:java][:install_flavor]                        = 'oracle'
-set[:java][:jdk_version]                           = '7'
-set[:java][:oracle][:accept_oracle_download_terms] = true
-set[:java][:oracle_rpm][:type]                     = 'jdk'
