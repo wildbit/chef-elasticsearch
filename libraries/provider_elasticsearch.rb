fa45bd38afb0803842268af15f1c25bb62ab001b
Michael Carruthers <michael.carruthers@wildbit.com>
2015-03-16 16:42:52 -0700

    share common resources, remove default modules, inherit from Chef::Provider

--- libraries/provider_elasticsearch.rb
+++ libraries/provider_elasticsearch.rb
@@ -1,175 +1,167 @@
-require          'chef/provider/lwrp_base'
+require          'chef/provider'
+require          'chef/dsl/recipe'
 require_relative 'helpers'
 
 class Chef
   class Provider
-    class Elasticsearch < LWRPBase
-      include Opscode::Elasticsearch::Helpers
+    class Elasticsearch < Chef::Provider
+      attr_reader :current
 
-      use_inline_resources
+      include ::Chef::DSL::Recipe
+      include ::Elasticsearch::Helpers
 
-      def whyrun_supported?
-        true
+      def shared_actions
+        # Install Java
+        package java_package
+
+        # Create group
+        group current.group do
+          gid current.gid
+        end
+
+        # Create user
+        user current.user do
+          group   current.group
+          gid     current.group
+          uid     current.uid
+          comment 'Elasticsearch'
+          shell   '/usr/bin/false'
+        end
+
+        # Create required directories
+        %w(data_dir home_dir log_dir work_dir).each do |dir|
+          dir = current.send(dir.to_sym)
+
+          directory dir do
+            owner     current.user
+            group     current.group
+            mode      '0775'
+            recursive true
+          end
+        end
+
+        # Fetch installation archive
+        remote_file installer_target do
+          owner    'root'
+          group    'root'
+          mode     '0755'
+          checksum current.checksum
+          source   current.source
+          backup   false
+          notifies :run, 'execute[decompress]', :immediately
+        end
+
+        # Decompress installation archive
+        execute 'decompress' do
+          action   :nothing
+          command  cmd_decompress
+          notifies :run, 'execute[permissions]', :immediately
+        end
+
+        # Set installation permissions
+        execute 'permissions' do
+          action :nothing
+          command cmd_permissions
+        end
+
+        # Create configuration directory
+        directory ::File.dirname(current.config_file) do
+          owner     'root'
+          group     'root'
+          mode      '0755'
+          recursive true
+        end
+
+        # Create & populate configuration file
+        template current.config_file do
+          owner    'root'
+          group    'root'
+          mode     '0644'
+          cookbook 'elasticsearch_lwrp'
+          backup   false
+          variables(
+            cluster:        current.cluster,
+            host:           current.listen,
+            http:           current.http,
+            http_port:      current.http_port,
+            members:        cluster_members,
+            marvel:         current.marvel,
+            mlockall:       current.mlockall,
+            modules:        current.modules,
+            multicast:      current.multicast,
+            name:           node.name,
+            transport_port: current.transport_port,
+            type:           current.type,
+            unicast:        current.unicast
+          )
+        end
+
+        # Log configuration
+        template current.log_config do
+          owner    current.user
+          group    current.group
+          mode     '0644'
+          cookbook 'elasticsearch_lwrp'
+          backup   false
+          variables(
+            log_file:  current.log_file,
+            log_level: current.log_level
+          )
+        end
+
+        # PID file directory
+        directory ::File.dirname(current.pid_file) do
+          owner    current.user
+          group    current.group
+          mode     '0755'
+          recursive true
+        end
+
+        # Install Marvel monitoring plugin
+        elasticsearch_plugin 'elasticsearch/marvel/latest' do
+          plugin_binary ::File.join(current.home_dir, 'bin/plugin')
+          only_if       { current.marvel }
+        end
       end
 
-      # Interface to bind to for connections
-      # @return [String] Interface to bind to
-      def interface
-        new_resource.address || 'net1'
-      end
+      def load_current_resource
+        @current = Chef::Resource::Elasticsearch.new(new_resource.name)
+        current.data_dir       new_resource.data_dir
+        current.home_dir       new_resource.home_dir
+        current.log_dir        new_resource.log_dir
+        current.work_dir       new_resource.work_dir
+        current.cluster        new_resource.cluster
+        current.checksum       new_resource.checksum
+        current.config_file    new_resource.config_file
+        current.gid            new_resource.gid
+        current.group          new_resource.group
+        current.http           new_resource.http
+        current.http_port      new_resource.http_port
+        current.java_heap      new_resource.java_heap
+        current.java_home      new_resource.java_home
+        current.java_version   new_resource.java_version
+        current.java_stack     new_resource.java_stack
+        current.listen         new_resource.listen
+        current.log_config     new_resource.log_config
+        current.log_file       new_resource.log_file
+        current.log_level      new_resource.log_level
+        current.marvel         new_resource.marvel
+        current.mlockall       new_resource.mlockall
+        current.members        new_resource.members
+        current.modules        new_resource.modules
+        current.multicast      new_resource.multicast
+        current.pid_file       new_resource.pid_file
+        current.resources      new_resource.resources
+        current.service_name   new_resource.service_name
+        current.source         new_resource.source
+        current.transport_port new_resource.transport_port
+        current.type           new_resource.type
+        current.user           new_resource.user
+        current.uid            new_resource.uid
+        current.unicast        new_resource.unicast
+        current.version        new_resource.version
 
-      # Cluster name
-      # @return [string] Cluster name
-      def cluster
-        new_resource.cluster || 'development'
-      end
-
-      # User to execute process as
-      # @return [String] Elasticsearch user
-      def es_user
-        new_resource.user || 'elasticsearch'
-      end
-
-      # Group to execute process as
-      # @return [String] Elasticsearch group
-      def es_group
-        new_resource.group || 'elasticsearch'
-      end
-
-      # UID of group
-      # @return [String] User UID
-      def es_uid
-        new_resource.uid || '700'
-      end
-
-      # GID of user
-      # @return [String] User GID
-      def es_gid
-        new_resource.gid || '700'
-      end
-
-      # Directory containing Java class files
-      # @return [String] Java class files directory
-      def class_dir
-        ::File.join(home_dir, 'lib')
-      end
-
-      # Directory containing configuration files
-      # @return [String] Configuration directory
-      def config_dir
-        ::File.join(home_dir, 'config')
-      end
-
-      # Directory containing log files
-      # @return [String] Log file directory
-      def log_dir
-        new_resource.log_dir || '/var/log/elasticsearch'
-      end
-
-      # Directory containing installed plugins
-      # @return [String] Plugin directory
-      def plugin_dir
-        ::File.join(home_dir, 'plugins')
-      end
-
-      # Port accepting HTTP connections
-      # @return [String] HTTP port
-      def http_port
-        new_resource.http_port || '9200-9300'
-      end
-
-      # Logging verbosity
-      # @return [String] Logging verbosity
-      def log_level
-        new_resource.log_level || 'DEBUG'
-      end
-
-      # Marvel monitoring agent
-      # @return [FalseClass, NilClass, TrueClass] Marvel agent
-      def marvel
-        new_resource.marvel || true
-      end
-
-      # Maximum amount of locked memory
-      # @return [String] Locked memory limit
-      def max_locked_memory
-        new_resource.max_locked_memory || 'unlimited'
-      end
-
-      # Maximum count of memory mapped files
-      # @return [Fixnum,String] Maximum memory mapped files
-      def max_memory_map
-        new_resource.max_memory_map || '262144'
-      end
-
-      # Maximum number of files open
-      # @return [String] Maximum allowed number of open files
-      def max_open_files
-        new_resource.max_open_files || '65535'
-      end
-
-      # Modules
-      # @return [Hash] Common module defaults
-      def modules
-        default_modules = {
-          'discovery.zen.ping.timeout'                    => '60s',
-          'discovery.zen.fd.ping_interval'                => '5s',
-          'discovery.zen.fd.ping_retries'                 => '5',
-          'discovery.zen.fd.ping_timeout'                 => '60s',
-          'index.merge.policy.max_merge_at_once'          => '8',
-          'index.merge.policy.max_merge_at_once_explicit' => '25',
-          'index.merge.scheduler.max_thread_count'        => '1',
-          'index.refresh_interval'                        => '5s',
-          'index.translog.flush_threshold_period'         => '5s',
-          'indices.memory.index_buffer_size'              => '15%',
-          'transport.tcp.connect_timeout'                 => '90s'
-        }
-        all_modules = default_modules.merge(new_resource.modules)
-
-        # Delete modules exposed via resource
-        %w[
-           discovery.zen.ping.multicast.enabled
-           discovery.zen.ping.unicast.hosts
-           transport.tcp.port
-        ].each { |mod| all_modules.delete!(mod) if default_modules.has_key?(mod) }
-
-        all_modules
-      end
-
-      # Multicast node discovery
-      # @return [FalseClass,TrueClass] Multicast discovery
-      def multicast
-        new_resource.multicast || false
-      end
-
-      # Port accepting HTTP connections
-      # @return [String] HTTP port
-      def http_port
-        new_resource.http_port || '9200'
-      end
-
-      # Service name
-      # @return [String] Service name
-      def service_name
-        new_resource.service_name || 'elasticsearch'
-      end
-
-      # Type of Elasticsearch member
-      # @return [String] Member type
-      def type
-        new_resource.type || 'data'
-      end
-
-      # Unicast node discovery
-      # @return [FalseClass,TrueClass] Unicast discovery
-      def unicast
-        new_resource.unicast || true
-      end
-
-      action :create do
-      end
-
-      action :create do
+        current
       end
     end
   end
