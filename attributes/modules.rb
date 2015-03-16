697893b73e7a0c2cd778538f6cad70c8c607c1a5
Michael Carruthers <michael.carruthers@wildbit.com>
2015-03-16 16:38:12 -0700

    split out modules to dedicated file

---
+++ attributes/modules.rb
@@ -0,0 +1,13 @@
+default[:elasticsearch][:modules] = {
+  'discovery.zen.ping.timeout'                    => '60s',
+  'discovery.zen.fd.ping_interval'                => '5s',
+  'discovery.zen.fd.ping_retries'                 => '5',
+  'discovery.zen.fd.ping_timeout'                 => '60s',
+  'index.merge.policy.max_merge_at_once'          => '8',
+  'index.merge.policy.max_merge_at_once_explicit' => '25',
+  'index.merge.scheduler.max_thread_count'        => '1',
+  'index.refresh_interval'                        => '5s',
+  'index.translog.flush_threshold_period'         => '5s',
+  'indices.memory.index_buffer_size'              => '15%',
+  'transport.tcp.connect_timeout'                 => '90s'
+}
