000f03d08de7075402f7daf46916c064fedf7ae7
Michael Carruthers <michael.carruthers@wildbit.com>
2015-03-27 14:38:25 -0700

    Correct resource action to allow ChefSpec to pass

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,8 +2,9 @@ require_relative 'spec_helper'
 
 describe 'elasticsearch_lwrp::default' do
   let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
- 
+  let(:node)     { chef_run.node }
+
   it 'will install elasticsearch' do
-    expect(chef_run).to create_elasticsearch('1.4.4')
+    expect(chef_run).to install_elasticsearch(node.elasticsearch.version)
   end
 end
