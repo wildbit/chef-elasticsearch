52753e6cef0c4bc575abfc6bc2485002827aa37f
Michael Carruthers <michael.carruthers@wildbit.com>
2015-04-13 17:50:30 -0700

    require LWRP libraries

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+require_relative '../libraries/helpers.rb'
+require_relative '../libraries/resource_elasticsearch.rb'
+require_relative '../libraries/provider_elasticsearch.rb'
+require_relative '../libraries/provider_elasticsearch_rhel.rb'
+require_relative '../libraries/provider_elasticsearch_smartos.rb'
 
 RSpec.configure do |config|
   config.platform = 'smartos'
