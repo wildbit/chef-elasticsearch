e69128a20ca81d4f4ff941341933564e1544fcbe
Michael Carruthers <michael.carruthers@wildbit.com>
2015-04-28 17:50:52 -0700

    additional clean up related to renaming of marvel to monitor attribute

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ description      'Install & configure Elasticsearch'
 license          'Apache 2.0'
 maintainer       'Wildbit LLC'
 maintainer_email 'sys-team@wildbit.com'
-version          '0.1.170'
+version          '0.1.171'
 
 %w(resource-control).each do |dep|
   depends dep
