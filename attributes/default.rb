471a0e5519a43312035ef0950f8a7dde8ad0fe30
Michael Carruthers <michael.carruthers@wildbit.com>
2015-05-04 14:54:40 -0700

    interface attribute updated supporting both network and transport interfaces

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default[:elasticsearch][:checksum]         = 'ffe2e46ec88f4455323112a556adaaa085669d13'
+default[:elasticsearch][:checksum]         = 'efae7897367ac3fa8057d02fad31c72e215b6edef599b63e373d3ce0c1049a14'
 default[:elasticsearch][:cluster]          = 'example'
 default[:elasticsearch][:group][:name]     = 'elasticsearch'
 default[:elasticsearch][:group][:gid]      = '700'
@@ -20,26 +20,26 @@ default[:elasticsearch][:version]          = '1.5.2'
 
 case node[:platform_family]
 when 'rhel'
-  default[:elasticsearch][:dir][:data]     = '/var/lib/elasticsearch'
-  default[:elasticsearch][:dir][:home]     = '/opt/elasticsearch'
-  default[:elasticsearch][:file][:pid]     = '/var/run/elasticsearch/elasticsearch.pid'
-  default[:elasticsearch][:interface]      = 'eth1'
-  default[:elasticsearch][:java][:home]    = '/usr'
-  default[:elasticsearch][:java][:version] = '1.8.0'
-  default[:elasticsearch][:resources]      = {
+  default[:elasticsearch][:dir][:data]        = '/var/lib/elasticsearch'
+  default[:elasticsearch][:dir][:home]        = '/opt/elasticsearch'
+  default[:elasticsearch][:file][:pid]        = '/var/run/elasticsearch/elasticsearch.pid'
+  default[:elasticsearch][:iface][:network]   = 'eth0'
+  default[:elasticsearch][:iface][:transport] = 'eth1'
+  default[:elasticsearch][:java][:home]       = '/usr'
+  default[:elasticsearch][:java][:version]    = '1.8.0'
+  default[:elasticsearch][:resources]         = {
     files:  { open:  '40000' },
     memory: { lock: 'unlimited', map: '262144' }
   }
 when 'smartos'
-  default[:elasticsearch][:dir][:data]     = '/var/db/elasticsearch'
-  default[:elasticsearch][:dir][:home]     = '/opt/local/elasticsearch'
-  default[:elasticsearch][:interface]      = 'net1'
-  default[:elasticsearch][:file][:pid]     = '/var/tmp/elasticsearch/elasticsearch.pid'
-  default[:elasticsearch][:java][:version] = '1.7.6'
-  default[:elasticsearch][:java][:home]    = "/opt/local/java/openjdk#{node[:elasticsearch][:java][:version].split('.')[1]}"
-  default[:elasticsearch][:resources]      = {
-    files:  { open: '40000' }
-  }
+  default[:elasticsearch][:dir][:data]        = '/var/db/elasticsearch'
+  default[:elasticsearch][:dir][:home]        = '/opt/local/elasticsearch'
+  default[:elasticsearch][:iface][:network]   = 'net0'
+  default[:elasticsearch][:iface][:transport] = 'net1'
+  default[:elasticsearch][:file][:pid]        = '/var/tmp/elasticsearch/elasticsearch.pid'
+  default[:elasticsearch][:java][:version]    = '1.7.6'
+  default[:elasticsearch][:java][:home]       = "/opt/local/java/openjdk#{node[:elasticsearch][:java][:version].split('.')[1]}"
+  default[:elasticsearch][:resources]         = { files:  { open: '40000' } }
 end
 default[:elasticsearch][:dir][:classes] = "#{node[:elasticsearch][:dir][:home]}/lib"
 default[:elasticsearch][:dir][:log]     = '/var/log/elasticsearch'
